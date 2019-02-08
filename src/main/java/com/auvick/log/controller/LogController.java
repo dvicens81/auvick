727f13ad4c5412b79520eb1ca5a0792184969e2e
Vicens Noguera David (ERNI) <noda@erni-espana.es>
2019-02-08 13:28:52 +0100

    Change RequestMapping to RequestMapping to vulnerability

--- src/main/java/com/auvick/log/controller/LogController.java
+++ src/main/java/com/auvick/log/controller/LogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +25,13 @@ public class LogController {
 		this.logService = logService;
 	}
 
-	@RequestMapping(value="/log")
+	@GetMapping(value="/log")
     public ResponseEntity<List<LogDto>> getLogsByStatusCode(@RequestParam("statusCode") int statusCode) {
 		List<LogDto> lLogsDto = logService.getLogsByStatusCode(statusCode);
 		return new ResponseEntity<List<LogDto>>(lLogsDto, HttpStatus.OK);
     }
 	
-	@RequestMapping("/request")
+	@GetMapping("/request")
 	public ResponseEntity<Page<LogDto>> getRequestByHostName(@RequestParam("hostName") String hostName) {
 		Page<LogDto> lLogsDto = logService.getLogsByHostName(hostName);
 		return new ResponseEntity<Page<LogDto>>(lLogsDto, HttpStatus.OK);
